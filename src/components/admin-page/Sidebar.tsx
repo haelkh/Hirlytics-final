42739b83a67114da03f1e649987e9bd35db127c5
haelkh <elkhatibhassan42@gmail.com>
2025-07-16 14:20:34 +0300

    Update index.html, Footer component, and Sidebar styles for improved branding and navigation
    - Changed favicon link in index.html to point to a new logo.
    - Updated social media links in Footer component to direct to actual profiles and added target attributes for better user experience.
    - Refactored Sidebar component to use NavLink for navigation, enhancing routing functionality and user interaction.
    - Improved Sidebar CSS styles for better layout and visual consistency.

--- src/components/admin-page/Sidebar.tsx
+++ src/components/admin-page/Sidebar.tsx
@@ -1,34 +1,48 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
 import dashboardIcon from "../../assets/dashboard.png";
 import peopleIcon from "../../assets/people.png";
 import assignmentIcon from "../../assets/assignment.png";
 import manageIcon from "../../assets/manage.png";
-import vectorIcon from "../../assets/vector.png";
+import vectorIcon from "../../assets/Vector.png";
 import jobSeekerIcon from "../../assets/job-seeker 1.png";
 import shapeIcon from "../../assets/Shape.png";
 import logoutIcon from "../../assets/logout.png";
 
 const Sidebar = () => {
   const menuItems = [
-    { id: 1, icon: dashboardIcon, label: "Dashboard" },
-    { id: 2, icon: peopleIcon, label: "Blog Post" },
-    { id: 3, icon: assignmentIcon, label: "Manage Users" },
-    { id: 4, icon: manageIcon, label: "Manage Applications" },
-    { id: 5, icon: vectorIcon, label: "Calendar" },
-    { id: 6, icon: jobSeekerIcon, label: "Jobs" },
-    { id: 7, icon: shapeIcon, label: "team" },
-    { id: 8, icon: logoutIcon, label: "Logout" },
+    { id: 1, icon: dashboardIcon, label: "Dashboard", path: "/AdminPage" },
+    { id: 2, icon: peopleIcon, label: "Blog Post", path: "/adminBlog" },
+    {
+      id: 3,
+      icon: assignmentIcon,
+      label: "Manage Users",
+      path: "/AdminPage/Manage/users",
+    },
+    { id: 4, icon: manageIcon, label: "Manage Applications", path: "#" },
+    {
+      id: 5,
+      icon: vectorIcon,
+      label: "Calendar",
+      path: "/AdminPage/Manage/calendar",
+    },
+    { id: 6, icon: jobSeekerIcon, label: "Jobs", path: "/Jobs" },
+    { id: 7, icon: shapeIcon, label: "team", path: "/AdminPage/Manage/team" },
+    { id: 8, icon: logoutIcon, label: "Logout", path: "#" },
   ];
 
   return (
     <div className="admin-sidebar">
       {menuItems.map((item) => (
-        <div
+        <NavLink
+          to={item.path}
           key={item.id}
-          className={`admin-sidebar-item ${
-            item.label === "Dashboard" ? "admin-sidebar-active" : ""
-          }`}
+          className={({ isActive }) =>
+            isActive
+              ? "admin-sidebar-item admin-sidebar-active"
+              : "admin-sidebar-item"
+          }
         >
           <img
             src={item.icon}
@@ -36,7 +50,7 @@ const Sidebar = () => {
             className="admin-sidebar-icon"
           />
           <span className="admin-sidebar-label">{item.label}</span>
-        </div>
+        </NavLink>
       ))}
     </div>
   );
